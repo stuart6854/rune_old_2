d1430cae205a8c8a9ea17ffc76aa516edc97adf0
Stuart Millman <stu.millman15@gmail.com>
2023-06-28 07:29:19 +0100

    refactor io::read_bytes()

--- Rune/include/rune/io/io.hpp
+++ Rune/include/rune/io/io.hpp
@@ -6,6 +6,8 @@
 #include "internal_common.hpp"
 
 #include <string>
+#include <fstream>
+#include <istream>
 #include <expected>
 #include <filesystem>
 
@@ -14,7 +16,25 @@ namespace rune::io
 #pragma region Read-Only
 
     auto read_string(const std::filesystem::path& filename) -> std::expected<std::string, std::string>;
-    auto read_bytes(const std::filesystem::path& filename) -> std::expected<std::vector<u8>, std::string>;
+
+    template <typename T>
+    auto read_binary(const std::filesystem::path& filename) -> std::expected<std::vector<T>, std::string>
+    {
+        auto file = std::ifstream(filename, std::ios::binary);
+        if (!file)
+        {
+            return std::unexpected("Failed to open file: " + filename.string());
+        }
+
+        file.seekg(0, std::ios::end);
+        const auto fileSize = file.tellg();
+        file.seekg(0, std::ios::beg);
+
+        std::vector<T> buffer(fileSize / sizeof(T));
+        file.read((char*)buffer.data(), fileSize);
+
+        return buffer;
+    }
 
 #pragma endregion
 
--- Rune/src/io/io.cpp
+++ Rune/src/io/io.cpp
@@ -19,22 +19,4 @@ namespace rune::io
         return buffer.str();
     }
 
-    auto read_bytes(const std::filesystem::path& filename) -> std::expected<std::vector<u8>, std::string>
-    {
-        auto file = std::ifstream("file.txt", std::ios::binary);
-        if (!file)
-        {
-            return std::unexpected("Failed to open file: " + filename.string());
-        }
-
-        file.seekg(0, std::ios::end);
-        const auto fileSize = file.tellg();
-        file.seekg(0, std::ios::beg);
-
-        std::vector<u8> buffer{};
-        buffer.reserve(fileSize);
-        buffer.insert(buffer.begin(), std::istream_iterator<u8>(file), std::istream_iterator<u8>());
-
-        return buffer;
-    }
 }
