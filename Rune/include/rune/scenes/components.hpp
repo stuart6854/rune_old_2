8322fc16565cce662c6363e5b2cd9c703addc3c7
Stuart Millman <stu.millman15@gmail.com>
2023-07-03 23:55:28 +0100

    Transform quaternion rotation + forward/up/right vectors

--- Rune/include/rune/scenes/components.hpp
+++ Rune/include/rune/scenes/components.hpp
@@ -1,11 +1,13 @@
 #pragma once
 
 #include "common.hpp"
+#include "platform/platform.hpp"
 #include "resources/handle.hpp"
 #include "graphics/renderer/static_mesh.hpp"
 #include "graphics/renderer/material.hpp"
 
 #include <glm/ext/vector_float3.hpp>
+#include <glm/ext/quaternion_float.hpp>
 
 #include <memory>
 
@@ -20,8 +22,13 @@ namespace rune::scenes
         DEFINE_COMPONENT_CTORS(Transform);
 
         glm::vec3 position{};
-        glm::vec3 eulerAngles{};
+        glm::quat rotation{};
+        //        glm::vec3 eulerAngles{};
         glm::vec3 scale{ 1, 1, 1 };
+
+        auto get_forward() const -> glm::vec3 { return rotation * glm::vec3{ 0, 0, 1 }; };
+        auto get_up() const -> glm::vec3 { return rotation * glm::vec3{ 0, 1, 0 }; };
+        auto get_right() const -> glm::vec3 { return rotation * glm::vec3{ 1, 0, 0 }; };
     };
 
     struct StaticRenderer
@@ -31,4 +38,12 @@ namespace rune::scenes
         resources::ResourceHandle<graphics::renderer::StaticMesh> mesh{ nullptr };
         std::vector<resources::ResourceHandle<graphics::renderer::Material>> materials{};
     };
+
+    struct Camera
+    {
+        DEFINE_COMPONENT_CTORS(Camera);
+
+        platform::WindowHandle targetWindow{};
+    };
+
 }
