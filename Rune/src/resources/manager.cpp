fe156dad731eaf5c8a881c8089c2d34d49c957e8
Stuart Millman <stu.millman15@gmail.com>
2023-07-03 10:51:23 +0100

    loading resources from disk

--- Rune/src/resources/manager.cpp
+++ Rune/src/resources/manager.cpp
@@ -20,12 +20,42 @@ namespace rune::resources
     {
         RUNE_ASSERT(g_managerData == nullptr);
         g_managerData = std::make_unique<ManagerData>();
+
+        const std::filesystem::path dataDirPath = "../../data";
+        LOG_INFO("resources - data dir: {}", std::filesystem::absolute(dataDirPath).string());
+        RUNE_ASSERT(std::filesystem::exists(dataDirPath));
+
+        for (const auto& entry : std::filesystem::recursive_directory_iterator("../../data"))
+        {
+            if (!entry.is_regular_file())
+            {
+                continue;
+            }
+
+            const auto& filePath = entry.path();
+            const auto& fileExt = filePath.extension();
+            if (fileExt == ".bin")
+            {
+                register_from_disk(filePath);
+            }
+        }
     }
 
     void shutdown()
     {
         RUNE_ASSERT(g_managerData != nullptr);
         g_managerData = nullptr;
+    }
+
+    void register_from_disk(const std::filesystem::path& filename)
+    {
+        const strid resourceId = STRID(filename.stem().string());
+        auto& metadata = g_managerData->metadataMap[resourceId];
+        metadata.source = ResourceSource::eDisk;
+        metadata.state = ResourceState::NotLoaded;
+        metadata.sourceFilename = filename;
+
+        LOG_INFO("resources - Resource registered from disk: id={}, filename={}", resourceId, filename.string());
     }
 
     void add_resource(ResourceId id, std::unique_ptr<Resource>&& resource)
