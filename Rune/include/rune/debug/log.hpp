a66e4b3630a9f187bb367132c0c2fe8248148012
Stuart Millman <stu.millman15@gmail.com>
2023-07-18 19:40:33 +0100

    glm vector logging

--- Rune/include/rune/debug/log.hpp
+++ Rune/include/rune/debug/log.hpp
@@ -2,6 +2,10 @@
 
 #include "rune/common.hpp"
 
+#include <glm/vec2.hpp>
+#include <glm/vec3.hpp>
+#include <glm/vec4.hpp>
+
 #include <format>
 #include <string_view>
 
@@ -36,3 +40,39 @@ namespace rune::debug
 #define RUNE_CLIENT_WARN(...) ::rune::debug::log_engine(__FILE__, __LINE__, ::rune::debug::LogLevel::Warn, __VA_ARGS__)
 #define RUNE_CLIENT_ERROR(...) ::rune::debug::log_engine(__FILE__, __LINE__, ::rune::debug::LogLevel::Error, __VA_ARGS__)
 #define RUNE_CLIENT_CRITICAL(...) ::rune::debug::log_engine(__FILE__, __LINE__, ::rune::debug::LogLevel::Critical, __VA_ARGS__)
+
+template <>
+struct std::formatter<glm::vec2> : std::formatter<std::string>
+{
+    auto format(const glm::vec2& v, format_context& ctx) { return std::format_to(ctx.out(), "[{}, {}]", v.x, v.y); }
+};
+
+template <>
+struct std::formatter<glm::vec3> : std::formatter<std::string>
+{
+    auto format(const glm::vec3& v, format_context& ctx) { return std::format_to(ctx.out(), "[{}, {}, {}]", v.x, v.y, v.z); }
+};
+
+template <>
+struct std::formatter<glm::vec4> : std::formatter<std::string>
+{
+    auto format(const glm::vec4& v, format_context& ctx) { return std::format_to(ctx.out(), "[{}, {}, {}, {}]", v.x, v.y, v.z, v.w); }
+};
+
+template <>
+struct std::formatter<glm::ivec2> : std::formatter<std::string>
+{
+    auto format(const glm::ivec2& v, format_context& ctx) { return std::format_to(ctx.out(), "[{}, {}]", v.x, v.y); }
+};
+
+template <>
+struct std::formatter<glm::ivec3> : std::formatter<std::string>
+{
+    auto format(const glm::ivec3& v, format_context& ctx) { return std::format_to(ctx.out(), "[{}, {}, {}]", v.x, v.y, v.z); }
+};
+
+template <>
+struct std::formatter<glm::ivec4> : std::formatter<std::string>
+{
+    auto format(const glm::ivec4& v, format_context& ctx) { return std::format_to(ctx.out(), "[{}, {}, {}, {}]", v.x, v.y, v.z, v.w); }
+};
